826adbcf8413b715067425a0d067ac4736230a8f
Elis Hirwing <elis@hirwing.se>
2019-05-26 23:22:18 +0200

    Update the dirlist script with some styles from bettermotherfuckingwebsite.com

--- .travis/make_document_dirlist.sh
+++ .travis/make_document_dirlist.sh
@@ -8,12 +8,20 @@ if ! test -d $DIR; then
     exit 1
 fi
 
-htmlHeader="<ul>"
-htmlFooter="</ul>"
+htmlHeader="<!DOCTYPE HTML>
+<html>
+  <head>
+    <title>Index of DIRPLACEHOLDER</title>
+    <style>body{margin:40px auto;max-width:650px;line-height:1.6;font-size:18px;color:#444;padding:0 10px}h1,h2,h3{line-height:1.2}</style>
+  </head>
+  <body>
+    <h1>Index of DIRPLACEHOLDER</h1>
+    <ul>"
+htmlFooter="</ul></body></html>"
 
 # Loop through all directories
 for directory in $(find $DIR -type d); do
-    html=$htmlHeader
+    html=$(echo $htmlHeader | sed "s~DIRPLACEHOLDER~$directory~g")
 
     for file in $(ls -a $directory); do
         html=$html"<li><a href=\"$file\">$file</a></li>"
